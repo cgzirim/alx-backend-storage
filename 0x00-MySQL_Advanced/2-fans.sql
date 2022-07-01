cb4ae4ade94731ed7a6d8820539f7b803d13c4c5
Chigozirim <iamokwukwe@gmail.com>
2022-07-01 23:23:40 +0100

    Update file

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,2 +1,2 @@
 -- Ranks country origins of bands, ordered by the number of (non-unique) fans
-SELECT origin, fans from holberton.metal_bands ORDER BY fans DESC;
+SELECT origin, fans AS nb_fans from holberton.metal_bands ORDER BY nb_fans DESC;

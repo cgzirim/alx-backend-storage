25180ad1fc34ef44e1c3d709dddca8396d5eac81
Chigozirim <iamokwukwe@gmail.com>
2022-07-02 01:22:55 +0100

    Make script executable on any database

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,5 @@
 -- List all bands with 'Glam rock' as their main style, ranked by their longevity.
-SELECT band_name, (formed - split) AS lifespan FROM holberton.metal_bands
-	WHERE style = 'Glam rock';
+SELECT band_name, (formed - split) AS lifespan 
+FROM metal_bands
+WHERE style = 'Glam rock';
+

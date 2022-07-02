5b1019b73689834b0efd03301f1cb933da0377b1
Chigozirim <iamokwukwe@gmail.com>
2022-07-02 01:14:07 +0100

    Correct script

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,2 +1,6 @@
--- Ranks country origins of bands, ordered by the number of (non-unique) fans
-SELECT origin, fans AS nb_fans from holberton.metal_bands ORDER BY nb_fans DESC;
+-- Ranks country origins of bands, ordered by the number of (non-unique) fans.
+SELECT origin, SUM(fans) AS nb_fans
+FROM holberton.metal_bands
+GROUP BY origin;
+ORDER BY nb_fans DESC;
+

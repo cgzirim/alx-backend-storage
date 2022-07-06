a954d150ba75b5f9c47ebaf70f0faf8d1906624a
Chigozirim <iamokwukwe@gmail.com>
2022-07-06 19:54:43 +0100

    Do task 0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -13,7 +13,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, int, bytes, float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """Generates random key and store the arg data in it."""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
